bcdcf08966a3c1c0f5f98574bb5b37e5caaa114d
Tim Nordin <74505667+Sidus-96@users.noreply.github.com>
2024-12-13 00:25:12 +0100

    carousell fix

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import burger1 from '../media_components/burger-3.png'
-import burger2 from '../media_components/burger-4.png'
-import burger3 from '../media_components/burger-5.png'
+import burger1 from '../media_components/burger-3.png';
+import burger2 from '../media_components/burger-4.png';
+import burger3 from '../media_components/burger-5.png';
 import { Image } from 'react-bootstrap';
 import './carousel.css';
 
-function burgerCarousel() {
+function BurgerCarousel() {
     return (
         <section>
             <div className="d-flex flex-column justify-content-center align-items-center" style={{ padding: '20px' }}>
-              
                 <h3>Våra bästsäljare</h3>
-                <style> {`.carousel-control-prev, .carousel-control-next {   display: none;`}</style>
+                <style>{`
+                    .carousel-control-prev, .carousel-control-next { display: none; }
+                `}</style>
                 <Carousel interval={3800}>
                     <Carousel.Item>
-                    <div className="d-flex justify-content-center align-items-center">
-                    <Image src={burger1} roundedCircle width="500" />
-    </div>
+                        <div className="carousel-image-wrapper">
+                            <Image src={burger1} roundedCircle className="carousel-image" />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                    <div className="d-flex justify-content-center align-items-center">
-                    <Image src={burger2} roundedCircle width="500" />
-    </div>
+                        <div className="carousel-image-wrapper">
+                            <Image src={burger2} roundedCircle className="carousel-image" />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                    <div className="d-flex justify-content-center align-items-center">
-     
-      <Image src={burger3}  roundedCircle width="500" />
-    </div>
+                        <div className="carousel-image-wrapper">
+                            <Image src={burger3} roundedCircle className="carousel-image" />
+                        </div>
                     </Carousel.Item>
                 </Carousel>
-             </div>
+            </div>
         </section>
     );
 }
 
-export default burgerCarousel;
+export default BurgerCarousel;
